08546728adeebb0aaf7fce1566070c9ae152f00e
Evan K <ekerik220@gmail.com>
2020-04-06 16:17:03 +0900

    Updates

---
+++ precache-manifest.04c250825c36c0472e211ed51ca0c1ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e09adf545ce81ede3be8d38228eca58",
+    "url": "/recipe-search/index.html"
+  },
+  {
+    "revision": "faa7ae7299f53e1b204c",
+    "url": "/recipe-search/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b52561d1313513018d33",
+    "url": "/recipe-search/static/css/main.11480c0d.chunk.css"
+  },
+  {
+    "revision": "faa7ae7299f53e1b204c",
+    "url": "/recipe-search/static/js/2.9ee6d9ca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/recipe-search/static/js/2.9ee6d9ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b52561d1313513018d33",
+    "url": "/recipe-search/static/js/main.3bab233c.chunk.js"
+  },
+  {
+    "revision": "a6266fd59a6f68d64d74",
+    "url": "/recipe-search/static/js/runtime-main.de634d92.js"
+  }
+]);
